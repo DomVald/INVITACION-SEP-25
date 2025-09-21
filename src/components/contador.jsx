9dad630310fb0fd7d4600cbfeb788e8290f48330
Dominic Valdes <doitfastyt@gmail.com>
2025-09-20 18:40:18 -0600

    se agregaron animaciones

--- src/components/contador.jsx
+++ src/components/contador.jsx
@@ -32,23 +32,23 @@ export default function Contador({ targetDate }) {
 
   return (
     <section className="w-full flex flex-col items-center justify-center py-16 px-6 bgx-secondary text-white">
-      <h2 className="text-3xl sm:text-4xl font-serif mb-6 text-center">
+      <h2 className="text-3xl block sm:text-4xl font-serif mb-6 text-center">
         Fecha: <br /> 1 de Diciembre de 2025
       </h2>
-      <h2 className="text-3xl sm:text-4xl font-serif mb-6 text-center">
+      <h2 className="text-3xl block sm:text-4xl font-serif mb-6 text-center">
         Solamente faltan:
       </h2>
 
       <div className="flex flex-row gap-4 sm:gap-8 text-center pb-10">
-        <div className="flex flex-col">
+        <div className="flex flex-col block">
           <span className="text-4xl sm:text-5xl font-serif font-bold">{timeLeft.days}</span>
           <span className="text-sm sm:text-base font-serif">Días</span>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col block">
           <span className="text-4xl sm:text-5xl font-serif font-bold">{timeLeft.hours}</span>
           <span className="text-sm sm:text-base font-serif">Horas</span>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col block">
           <span className="text-4xl sm:text-5xl font-serif font-bold">{timeLeft.minutes}</span>
           <span className="text-sm sm:text-base font-serif">Minutos</span>
         </div>
